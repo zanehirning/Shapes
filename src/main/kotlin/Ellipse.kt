3096cbcc44bc1a73bcfd70d0420e49df8331ad07
zanehirning <a02371568@usu.edu>
2024-07-01 19:37:56 -0600

    ellipse implementation

---
+++ src/main/kotlin/Ellipse.kt
@@ -0,0 +1,26 @@
+package org.example
+
+class Ellipse(
+    private var center: Point,
+    private var radiusX: Double,
+    private var radiusY: Double
+) : Shape {
+
+    init {
+        if (radiusX <= 0 || radiusY <= 0) {
+            throw IllegalArgumentException("Radii must be positive")
+        }
+    }
+
+    override fun getPoints(): List<Point> {
+        return listOf(center.clone())
+    }
+
+    override fun move(xDelta: Double, yDelta: Double) {
+        this.center.move(xDelta, yDelta)
+    }
+
+    override fun area(): Double {
+        return Math.PI * radiusX * radiusY
+    }
+}

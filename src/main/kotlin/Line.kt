6a6ba9bb86f3ab259da207ed64e39329e809617a
zanehirning <a02371568@usu.edu>
2024-07-01 19:31:11 -0600

    Line implementation

---
+++ src/main/kotlin/Line.kt
@@ -0,0 +1,34 @@
+package org.example
+
+class Line(
+    private var startPoint: Point,
+    private var endPoint: Point
+) {
+
+    init {
+        if (startPoint.getX() == endPoint.getX() && startPoint.getY() == endPoint.getY()) {
+            throw IllegalArgumentException("Start and end points must be different")
+        }
+    }
+
+    fun getStartPoint(): Point {
+        return this.startPoint.clone()
+    }
+
+    fun getEndPoint(): Point {
+        return this.endPoint.clone()
+    }
+
+    fun move(xDelta: Double, yDelta: Double) {
+        this.startPoint.move(xDelta, yDelta)
+        this.endPoint.move(xDelta, yDelta)
+    }
+
+    fun slope(): Double {
+        return (endPoint.getY() - startPoint.getY()) / (endPoint.getX() - startPoint.getX())
+    }
+
+    fun length(): Double {
+        return Math.sqrt(Math.pow(endPoint.getX() - startPoint.getX(), 2.0) + Math.pow(endPoint.getY() - startPoint.getY(), 2.0))
+    }
+}
